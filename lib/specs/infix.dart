6dbbd612f805dbd4b3abdf952d8ae80a97af7ad2
Didier Plaindoux <d.plaindoux@free.fr>
2023-11-12 20:54:53 +0100

    Update infix.dart

--- lib/specs/infix.dart
+++ lib/specs/infix.dart
@@ -1,10 +1,10 @@
 import 'package:feng/core/types.dart';
 import 'package:feng/specs/functor.dart';
 
-extension FunctorInfix<E extends Functor<M>, M> on E {
+extension InfixExtension<E extends Functor<M>, M> on E {
   (E, HKP<M, A>) infix<A>(HKP<M, A> ma) => (this, ma);
 }
 
-extension FunctorUsing<M, A> on HKP<M, A> {
+extension UsingExtension<M, A> on HKP<M, A> {
   (E, HKP<M, A>) using<E extends Functor<M>>(E e) => (e, this);
 }
